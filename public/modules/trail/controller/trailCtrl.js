9d3c0e0e4dbe59db5f15e92e2540dfcebfab8a94
Benedict Wee <benedictwee.2014@sis.smu.edu.sg>
2018-03-12 01:04:15 +0800

    Update QR Reader

--- public/modules/trail/controller/trailCtrl.js
+++ public/modules/trail/controller/trailCtrl.js
@@ -4,33 +4,11 @@ angular.module('TrailCtrl', ['appConstants'])
     document.getElementById("body_content").setAttribute('class', '');
     
     $(document).ready(function() {
-        navigator.getUserMedia = (navigator.getUserMedia ||
-            navigator.webkitGetUserMedia ||
-            navigator.mozGetUserMedia || 
-            navigator.msGetUserMedia);
-        if (navigator.getUserMedia) {
-            // Request the camera.
-            navigator.getUserMedia(
-              // Constraints
-              {
-                video: true
-              },
-          
-              // Success Callback
-              function(localMediaStream) {
-          
-              },
-          
-              // Error Callback
-              function(err) {
-                // Log the error to the console.
-                console.log('The following error occurred when trying to use getUserMedia: ' + err);
-              }
-            );
-          
-          } else {
-            alert('Sorry, your browser does not support getUserMedia');
-          }
+        if (hasGetUserMedia()) {
+            // Good to go!
+        } else {
+            alert('getUserMedia() is not supported by your browser');
+        }
         // TABS
         $('ul.tabs').tabs();
         $('.button-collapse').sideNav({
@@ -48,6 +26,10 @@ angular.module('TrailCtrl', ['appConstants'])
         $('.modal').modal();
         $('.tooltipped').tooltip({delay: 50});
     });
+
+    function hasGetUserMedia() {
+        return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+      }
 
     var vm = this;
 
