16c066a7f8f338390ddba6445e3a9e7b7107a04a
Benedict Wee <benedictwee.2014@sis.smu.edu.sg>
2018-03-12 00:52:13 +0800

    Update QR Reader

--- public/modules/trail/controller/trailCtrl.js
+++ public/modules/trail/controller/trailCtrl.js
@@ -8,6 +8,29 @@ angular.module('TrailCtrl', ['appConstants'])
             navigator.webkitGetUserMedia ||
             navigator.mozGetUserMedia || 
             navigator.msGetUserMedia);
+        if (navigator.getUserMedia) {
+            // Request the camera.
+            navigator.getUserMedia(
+              // Constraints
+              {
+                video: true
+              },
+          
+              // Success Callback
+              function(localMediaStream) {
+          
+              },
+          
+              // Error Callback
+              function(err) {
+                // Log the error to the console.
+                console.log('The following error occurred when trying to use getUserMedia: ' + err);
+              }
+            );
+          
+          } else {
+            alert('Sorry, your browser does not support getUserMedia');
+          }
         // TABS
         $('ul.tabs').tabs();
         $('.button-collapse').sideNav({
