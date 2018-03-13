b84e41fc1b9708c826866c98865c73ebac1b35b4
Benedict Wee <benedictwee.2014@sis.smu.edu.sg>
2018-03-14 01:44:15 +0800

    Update pop up

--- public/modules/trail/controller/trailCtrl.js
+++ public/modules/trail/controller/trailCtrl.js
@@ -197,7 +197,7 @@ angular.module('TrailCtrl', ['appConstants'])
               
                     });
 
-                    if(result[0] < 300){
+                    //if(result[0] < 300){
                         navigator.geolocation.clearWatch(vm.map.watchPosition);
                         console.log(result[1])
                         vm.location = {};
@@ -207,73 +207,6 @@ angular.module('TrailCtrl', ['appConstants'])
                         vm.location.hours = result[1].description.hours 
 
                         var video = document.createElement("video");
-    var canvasElement = document.getElementById("canvas");
-    var canvas = canvasElement.getContext("2d");
-    var loadingMessage = document.getElementById("loadingMessage");
-    var outputContainer = document.getElementById("output");
-    var outputMessage = document.getElementById("outputMessage");
-    var outputData = document.getElementById("outputData");
-
-    function drawLine(begin, end, color) {
-      canvas.beginPath();
-      canvas.moveTo(begin.x, begin.y);
-      canvas.lineTo(end.x, end.y);
-      canvas.lineWidth = 4;
-      canvas.strokeStyle = color;
-      canvas.stroke();
-    }
-
-    // Use facingMode: environment to attemt to get the front camera on phones
-    navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } }).then(function(stream) {
-      video.srcObject = stream;
-      video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
-      video.play();
-      requestAnimationFrame(tick);
-    });
-
-    function tick() {
-      loadingMessage.innerText = "⌛ Loading video..."
-      if (video.readyState === video.HAVE_ENOUGH_DATA) {
-        loadingMessage.hidden = true;
-        canvasElement.hidden = false;
-        outputContainer.hidden = false;
-
-        canvasElement.height = 250 //video.videoHeight;
-        canvasElement.width = 250 //video.videoWidth;
-        canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
-        var imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
-        var code = jsQR(imageData.data, imageData.width, imageData.height);
-        
-        if (code) {
-            if(code.data == "Tong Heng Pastries" && vm.read_once == 0){
-                drawLine(code.location.topLeftCorner, code.location.topRightCorner, "#FF3B58");
-                drawLine(code.location.topRightCorner, code.location.bottomRightCorner, "#FF3B58");
-                drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, "#FF3B58");
-                drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, "#FF3B58");
-                outputMessage.hidden = true;
-                outputData.parentElement.hidden = false;
-                outputData.innerText = code.data;
-
-                vm.read_once++;
-                $('#info_modal').modal('close');
-                $('.location-collapse').sideNav({
-                    menuWidth: (window.innerWidth < 500) ? window.innerWidth : 300,
-                    edge: 'right', // Choose the horizontal origin
-                    closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-                });
-                $('.location-collapse').sideNav('show');
-            }
-
-        } else {
-          outputMessage.hidden = false;
-          outputData.parentElement.hidden = true;
-        }
-      }
-      requestAnimationFrame(tick);
-    }
-
-
-                        /*var video = document.createElement("video");
                         var canvasElement = document.getElementById("canvas");
                         var canvas = canvasElement.getContext("2d");
                         var loadingMessage = document.getElementById("loadingMessage");
@@ -299,20 +232,20 @@ angular.module('TrailCtrl', ['appConstants'])
                         });
 
                         function tick() {
-                            loadingMessage.innerText = "⌛ Loading video..."
-                            if (video.readyState === video.HAVE_ENOUGH_DATA) {
-                                loadingMessage.innerText = "⌛ HIHI"
-                                loadingMessage.hidden = true;
-                                canvasElement.hidden = false;
-                                outputContainer.hidden = false;
+                        loadingMessage.innerText = "⌛ Loading video..."
+                        if (video.readyState === video.HAVE_ENOUGH_DATA) {
+                            loadingMessage.hidden = true;
+                            canvasElement.hidden = false;
+                            outputContainer.hidden = false;
 
-                                canvasElement.height = 250//video.videoHeight;
-                                canvasElement.width = 250//video.videoWidth;
-                                canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
-                                var imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
-                        
-                                var code = jsQR(imageData.data, imageData.width, imageData.height);
-                                if (code && code.data == "Tong Heng Pastries" && vm.read_once == 0) {
+                            canvasElement.height = 250 //video.videoHeight;
+                            canvasElement.width = 250 //video.videoWidth;
+                            canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+                            var imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
+                            var code = jsQR(imageData.data, imageData.width, imageData.height);
+                            
+                            if (code) {
+                                if(code.data == "Tong Heng Pastries" && vm.read_once == 0){
                                     drawLine(code.location.topLeftCorner, code.location.topRightCorner, "#FF3B58");
                                     drawLine(code.location.topRightCorner, code.location.bottomRightCorner, "#FF3B58");
                                     drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, "#FF3B58");
@@ -321,33 +254,32 @@ angular.module('TrailCtrl', ['appConstants'])
                                     outputData.parentElement.hidden = false;
                                     outputData.innerText = code.data;
 
-                                    
-                                        
-                                        console.log("OPENNN ME")
-                                        $('#info_modal').modal('close');
-                            
-                                        vm.read_once++;
-
-                                        $('.location-collapse').sideNav({
-                                            menuWidth: (window.innerWidth < 500) ? window.innerWidth : 300,
-                                            edge: 'right', // Choose the horizontal origin
-                                            closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-                                        });
-                                        $('.location-collapse').sideNav('show');
-                                    
-                                } else {
-                                    outputMessage.hidden = false;
-                                    outputData.parentElement.hidden = true;
+                                    vm.read_once++;
+                                    $('#info_modal').modal('close');
+                                    $('.location-collapse').sideNav({
+                                        menuWidth: (window.innerWidth < 500) ? window.innerWidth : 300,
+                                        edge: 'right', // Choose the horizontal origin
+                                        closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+                                    });
+                                    $('.location-collapse').sideNav('show');
                                 }
+
+                            } else {
+                            outputMessage.hidden = false;
+                            outputData.parentElement.hidden = true;
                             }
-                            requestAnimationFrame(tick);
-                        }*/
+                        }
+                        requestAnimationFrame(tick);
+                        }
+
+
+                       
 
                         $('.modal').modal();
                         $('#info_modal').modal('open');
 
                         //watchPosition = navigator.geolocation.watchPosition(success, error, option);
-                    }
+                    //}
                 }
                 function error(err) {
                     console.warn('ERROR(' + err.code + '): ' + err.message);
