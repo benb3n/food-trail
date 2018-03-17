844101ceccf64c7375871c890be9a0d2054ef0f0
Benedict Wee <benedictwee.2014@sis.smu.edu.sg>
2018-03-17 22:11:15 +0800

    Testing AR

--- public/modules/arvideo/controller/arvideoCtrl.js
+++ public/modules/arvideo/controller/arvideoCtrl.js
@@ -23,14 +23,15 @@ angular.module('ArvideoCtrl', [])
         AFRAME.registerComponent('artoolkit', { init: function () { 
             var sceneEl = document.querySelector('a-scene').querySelector('a-assets'); 
             var video = sceneEl.querySelector('video'); 
-            var canvas = document.getElementsByClassName('a-canvas'); 
-            canvas[0].addEventListener('click', function () { console.log("YEAH HERE NOW") 
-                if (video.paused == true) { 
+            //var canvas = document.getElementsByClassName('a-assets'); 
+            //console.log(canvas)
+            //canvas[0].addEventListener('click', function () { console.log("YEAH HERE NOW") 
+                //if (video.paused == true) { 
                      video.play(); 
-                } else {  
-                    video.pause(); 
-                } 
-            }, false); 
+                //} else {  
+                //    video.pause(); 
+                //} 
+            //}, false); 
         } });
     }
 
